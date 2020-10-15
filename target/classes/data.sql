079a092ffef2123c79676947464cb2e37a97aeae
Artur <arturkubiak9@gmail.com>
2020-10-15 20:07:29 +0200

    Add three functionality

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -3,18 +3,18 @@ insert into Teams (team_id, name, country, city) values (2, 'Manchester United',
 insert into Teams (team_id, name, country, city) values (3, 'Borussia Dortmund', 'Germany', 'Dortmund');
 insert into Teams (team_id, name, country, city) values (4, 'Real Madrid', 'Spain', 'Madrid');
 insert into Teams (team_id, name, country, city) values (5, 'FC Barcelona', 'Spain', 'Barcelona');
-insert into Teams (team_id, name, country, city) values (6, 'Chelsea F.C.', 'United Kingdom', 'London');
+insert into Teams (team_id, name, country, city) values (6, 'Chelsea FC', 'United Kingdom', 'London');
 insert into Teams (team_id, name, country, city) values (7, 'Wisla Cracow', 'Poland', 'Cracow');
 insert into Teams (team_id, name, country, city) values (8, 'A.C. Milan', 'Italy', 'Milano');
-insert into Teams (team_id, name, country, city) values (9, 'Paris Saint-Germain F.C.', 'France', 'Paris');
-insert into Teams (team_id, name, country, city) values (10, 'Juventus F.C.', 'Italy', 'Torino');
+insert into Teams (team_id, name, country, city) values (9, 'Paris Saint-Germain FC', 'France', 'Paris');
+insert into Teams (team_id, name, country, city) values (10, 'Juventus FC', 'Italy', 'Torino');
 
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (101, '2020-08-26', 1, 2, 1, 0);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (102, '2020-07-15', 2, 5, 4, 0);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (103, '2020-09-11', 3, 10, 2, 3);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (104, '2020-07-07', 4, 6, 3, 1);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (105, '2020-08-12', 5, 8, 2, 0);
-insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (106, '2020-09-16', 6, 4, 0, 5);
+insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (106, '2020-09-16', 6, 4, 1, 5);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (107, '2020-08-07', 7, 1, 2, 4);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (108, '2020-07-24', 8, 9, 1, 1);
 insert into matches (match_id, date, home_team_id, away_team_id, goals_home, goals_away) values (109, '2020-09-03', 9, 7, 0, 0);
