404e90c762edd99d547ad4eea72a5ce5eb5a01a5
Artur <arturkubiak9@gmail.com>
2020-10-15 16:44:36 +0200

    Add structure of assigment

---
+++ src/main/java/com/example/demo/model/Match.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "matches")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "match_id")
+    private Long matchID;
+
+    private Date date;
+
+    @Column(name = "home_team_id")
+    private Long homeTeamID;
+
+    @Column(name = "away_team_id")
+    private Long awayTeamID;
+
+    @Column(name = "goals_home")
+    private int goalsHome;
+
+    @Column(name = "goals_away")
+    private int goalsAway;
+
+    public Match(Long matchID, Date date, Long homeTeamID, Long awayTeamID, int goalsHome, int goalsAway) {
+        this.matchID = matchID;
+        this.date = date;
+        this.homeTeamID = homeTeamID;
+        this.awayTeamID = awayTeamID;
+        this.goalsHome = goalsHome;
+        this.goalsAway = goalsAway;
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Team.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "teams")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "team_id")
+    private Long teamID;
+
+    private String name;
+    private String country;
+    private String city;
+
+    public Team(Long teamID, String name, String country, String city) {
+        this.teamID = teamID;
+        this.name = name;
+        this.country = country;
+        this.city = city;
+    }
+}
